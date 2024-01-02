769612877a4d7bf8ba2c1da0f159b21317a9bf07
KaalaBaloo <kaalabaloo@gmail.com>
2024-01-02 20:34:52 +0100

    gg

--- Assets/Scripts/Characters/MirrorOfC.cs
+++ Assets/Scripts/Characters/MirrorOfC.cs
@@ -91,9 +91,9 @@ public class MirrorOfC : Enemy
         _topRay.transform.position = new Vector2(transform.position.x, transform.position.y + hitUp.distance/2);
         _rightRay.transform.position = new Vector2(transform.position.x + hitRight.distance/2, transform.position.y);
         _downRay.transform.position = new Vector2(transform.position.x, transform.position.y - hitDown.distance/2);
-        _leftRay.GetComponent<SpriteRenderer>().size = new Vector2(hitLeft.distance * 11, _leftRay.GetComponent<SpriteRenderer>().size.y);
-        _topRay.GetComponent<SpriteRenderer>().size = new Vector2(_topRay.GetComponent<SpriteRenderer>().size.x, hitUp.distance * 5);
-        _rightRay.GetComponent<SpriteRenderer>().size = new Vector2(hitRight.distance * 11, _rightRay.GetComponent<SpriteRenderer>().size.y);
-        _downRay.GetComponent<SpriteRenderer>().size = new Vector2(_downRay.GetComponent<SpriteRenderer>().size.x, hitDown.distance * 5);
+        _leftRay.GetComponent<SpriteRenderer>().size = new Vector2(hitLeft.distance * 11.5f, _leftRay.GetComponent<SpriteRenderer>().size.y);
+        _topRay.GetComponent<SpriteRenderer>().size = new Vector2(_topRay.GetComponent<SpriteRenderer>().size.x, hitUp.distance * 11.5f);
+        _rightRay.GetComponent<SpriteRenderer>().size = new Vector2(hitRight.distance * 11.5f, _rightRay.GetComponent<SpriteRenderer>().size.y);
+        _downRay.GetComponent<SpriteRenderer>().size = new Vector2(_downRay.GetComponent<SpriteRenderer>().size.x, hitDown.distance * 11.5f);
     }
 }
